130d67f24369d5c188098a072cb43396d6c5d8e0
fahim2023 <fa733m4d4m@outlook.com>
2026-05-25 15:03:33 +0100

    add crispy forms and post request path logic to comment and submit comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -37,7 +37,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +50,8 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "django_summernote",
     "blog",
     "about",
@@ -58,6 +59,8 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 SUMMERNOTE_THEME = "bs4"
